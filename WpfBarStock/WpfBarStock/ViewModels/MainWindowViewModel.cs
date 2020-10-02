a766fadea07e392044b31b454da170f76ac0a32a
Milan992 <mitic.gsm@gmail.com>
2020-10-02 10:51:11 +0200

    Created LogIn xaml.

--- WpfBarStock/WpfBarStock/ViewModels/MainWindowViewModel.cs
+++ WpfBarStock/WpfBarStock/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace WpfBarStock.ViewModels
 {
     class MainWindowViewModel : ViewModelBase
     {
+        MainWindow main;
+
+        #region Constructors
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string userName;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand logIn;
+
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => LogInExecute(), param => CanLogInExecute());
+                }
+
+                return logIn;
+            }
+        }
+
+        private void LogInExecute()
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanLogInExecute()
+        {
+            return true;
+        }
+
+        #endregion
     }
+}
 }
